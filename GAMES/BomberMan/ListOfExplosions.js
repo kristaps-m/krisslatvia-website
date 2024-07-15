f2f945c5d419ef0187a759a593e2a2171cb0b637
kristaps mitins <professional17@inbox.lv>
2024-07-16 00:30:20 +0300

    u - delete enemy from field if it is inside explosion.

--- GAMES/BomberMan/ListOfExplosions.js
+++ GAMES/BomberMan/ListOfExplosions.js
@@ -1,9 +1,9 @@
-class ListOfExplosions{
-  constructor(){
+class ListOfExplosions {
+  constructor() {
     this.ListOfExplosions = [];
   }
 
-  checkExplosionTime(){
+  checkExplosionTime() {
     // CODE FROM ListOfBombs:
     //  listOfExplosions.ListOfExplosions.push({x:b.x,y:b.y,s:b.size,d:new Date()});
     let dateNow = new Date();
@@ -14,36 +14,41 @@ class ListOfExplosions{
         console.log("DELETE?? Exp");
       }
     }
-
   }
 
-  drawExplosions(){
+  drawExplosions() {
     this.checkExplosionTime();
     CTX.fillStyle = "pink";
     this.ListOfExplosions.forEach((e) => {
       // draw exposion/s:
+      // console.log(e);
+
       for (let row = 0; row < bombExlosion_2.length; row++) {
         for (let col = 0; col < bombExlosion_2[0].length; col++) {
           const fieldSquare = bombExlosion_2[row][col];
-          const squareX = e.x + bManRadius * row;
-          const squareY = e.y + bManRadius * col;
+          const squareX = e.x + bManRadius * row - bManRadius * 2;
+          const squareY = e.y + bManRadius * col - bManRadius * 2;
           if (fieldSquare === 1) {
             // Black square
-            CTX.fillRect(
-              squareX - bManRadius * 2,
-              squareY - bManRadius * 2,
-              bManRadius,
-              bManRadius
-            );
-            listOfEnemies.forEach((e,i) => {
-              if(e.x < squareX && e.y < squareY){
+            CTX.fillRect(squareX, squareY, bManRadius, bManRadius);
+            // list of enemies? if in explosion then delete?
+            listOfEnemies.forEach((oneEnemy, i) => {
+              if (
+                oneEnemy.x > squareX &&
+                oneEnemy.y > squareY &&
+                oneEnemy.x + bManRadius - 5 <= squareX + bManRadius &&
+                oneEnemy.y + bManRadius - 5 <= squareY + bManRadius
+              ) {
+                // oneEnemy.x < squareX && oneEnemy.y < squareY
                 console.log("enemy Inside Explosion!");
-                listOfEnemies.splice(i,1);
+                listOfEnemies.splice(i, 1);
               }
             });
+
+            // console.log(e);
           }
         }
       }
-    })
+    });
   }
 }
