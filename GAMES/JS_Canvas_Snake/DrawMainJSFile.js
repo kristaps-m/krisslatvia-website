82f79bfa002c743cfe8af79ac7561e1bb6e67315
kristaps mitins <professional17@inbox.lv>
2024-10-02 20:19:41 +0300

    create snake eyes for each direction

--- GAMES/JS_Canvas_Snake/DrawMainJSFile.js
+++ GAMES/JS_Canvas_Snake/DrawMainJSFile.js
@@ -1,15 +1,16 @@
-import {Snake} from './Snake.js';
+import { Snake } from "./Snake.js";
 import { Food } from "./Food.js";
 
 const canvas = document.getElementById("snake_canvas");
 const ctx = canvas.getContext("2d");
-let oneSquareSize = parseInt(document.getElementById('squareSize').value);
+let oneSquareSize = parseInt(document.getElementById("squareSize").value);
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 let isGameOver = false;
 let isPaused = false;
 let theGameFrameCount = 0;
-let gameFieldFullNumber = (canvasWidth / oneSquareSize) * (canvasHeight / oneSquareSize);
+let gameFieldFullNumber =
+  (canvasWidth / oneSquareSize) * (canvasHeight / oneSquareSize);
 let snake = new Snake(oneSquareSize, canvasWidth, canvasHeight);
 let food = new Food(oneSquareSize, gameFieldFullNumber);
 let isGridON = false;
@@ -18,106 +19,118 @@ let snakeIsMoved = false;
 let userTypingInPause = [];
 
 function theSnakeGameLoop() {
-  if(theGameFrameCount % 8 === 0){
-    if(isPaused){
-      displayText('    PAUSE    ');
+  if (theGameFrameCount % 8 === 0) {
+    if (isPaused) {
+      displayText("    PAUSE    ");
     }
-    if(gameFieldFullNumber - 1 === snake.tail.length){
-      var variableDisplay = document.getElementById('scoreDisplay').textContent;
-      displayText('YOU WON!' + ` score: ${variableDisplay}`);
+    if (gameFieldFullNumber - 1 === snake.tail.length) {
+      var variableDisplay = document.getElementById("scoreDisplay").textContent;
+      displayText("YOU WON!" + ` score: ${variableDisplay}`);
       isGameOver = true;
     }
-    if(snake.isSnakeHeadCrashedInTail() && snake.tail.length > 4 && snakeIsMoved){
-      var variableDisplay = document.getElementById('scoreDisplay').textContent;
-      displayText('GAME OVER!'  + ` score: ${variableDisplay}`);
+    if (
+      snake.isSnakeHeadCrashedInTail() &&
+      snake.tail.length > 4 &&
+      snakeIsMoved
+    ) {
+      var variableDisplay = document.getElementById("scoreDisplay").textContent;
+      displayText("GAME OVER!" + ` score: ${variableDisplay}`);
       isGameOver = true;
     }
-    if(!isGameOver && !isPaused){
+    if (!isGameOver && !isPaused) {
       the_draw();
     }
   }
   requestAnimationFrame(theSnakeGameLoop);
   theGameFrameCount++;
-  if(theGameFrameCount > Math.pow(10,6)){
+  if (theGameFrameCount > Math.pow(10, 6)) {
     theGameFrameCount = 0;
   }
 }
 
 theSnakeGameLoop();
 
-function the_draw(){
+function the_draw() {
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
   snake.update();
-  food.draw(ctx, 'black');
-  snake.drawTail(ctx, 'green');
-  snake.draw(ctx, 'red');
-  if(isGridON){
-    displayGrid({ctx:ctx, strokeStyle: "white", girdLineWidth: 1, canvasHeight: canvasHeight, canvasWidth: canvasWidth, oneSquareSize: oneSquareSize})
+  food.draw(ctx, "black");
+  snake.drawTail(ctx, "green");
+  snake.draw(ctx, "red");
+  if (isGridON) {
+    displayGrid({
+      ctx: ctx,
+      strokeStyle: "white",
+      girdLineWidth: 1,
+      canvasHeight: canvasHeight,
+      canvasWidth: canvasWidth,
+      oneSquareSize: oneSquareSize,
+    });
   }
 
-  if(food.isFoodEaten(snake.xLocation, snake.yLocation)){
-    food.createNewFood(ctx, 'black', canvasWidth, canvasHeight, snake.tail);
+  if (food.isFoodEaten(snake.xLocation, snake.yLocation)) {
+    food.createNewFood(ctx, "black", canvasWidth, canvasHeight, snake.tail);
     snake.updateTail();
-    var variableDisplay = document.getElementById('scoreDisplay');
+    var variableDisplay = document.getElementById("scoreDisplay");
     variableDisplay.textContent = parseInt(variableDisplay.textContent) + 1;
   }
   snake.removeFirstElementFromTail();
 }
 
-document.addEventListener('keydown', function (event) {
-  let theKeyPressed = event.key.replace('Arrow', '');
-  if(theKeyPressed === 'p' || theKeyPressed === ' '){
+document.addEventListener("keydown", function (event) {
+  let theKeyPressed = event.key.replace("Arrow", "");
+  if (theKeyPressed === "p" || theKeyPressed === " ") {
     isPaused = !isPaused;
   }
-  if(isPaused){
+  if (isPaused) {
     userTypingInPause.push(theKeyPressed);
     console.log(userTypingInPause);
   }
-  if(userTypingInPause.join('').includes("iamchuck")){
-    var variableDisplay = document.getElementById('scoreDisplay');
+  if (userTypingInPause.join("").includes("iamchuck")) {
+    var variableDisplay = document.getElementById("scoreDisplay");
     variableDisplay.textContent = parseInt(variableDisplay.textContent) + 10000;
-    userTypingInPause=[];
-  }else if (userTypingInPause.length > 50){
+    userTypingInPause = [];
+  } else if (userTypingInPause.length > 50) {
     userTypingInPause = [];
   }
   snake.changeDirection(theKeyPressed);
   snakeIsMoved = true;
-})
+});
 
-document.getElementById('toggleGirdOnOff').onclick = function() {
-    isGridON = !isGridON;
+document.getElementById("toggleGirdOnOff").onclick = function () {
+  isGridON = !isGridON;
 };
 
-function displayText(theText){
+function displayText(theText) {
   ctx.font = "italic bold 30px Comic Sans MS";
   ctx.textAlign = "center";
   // IF you want to display Rect under displayText you can do it :)
   // ctx.fillStyle = 'white';
   // ctx.fillRect(canvasWidth * 0.1, canvasHeight /2 - 50, canvasWidth * 0.9, 50);
-  ctx.fillStyle = 'blue';
-  ctx.fillText(theText, canvasWidth / 2, canvasHeight /2);
+  ctx.fillStyle = "blue";
+  ctx.fillText(theText, canvasWidth / 2, canvasHeight / 2);
 }
 
-document.getElementById('togglePauseGame').onclick = function() {
+document.getElementById("togglePauseGame").onclick = function () {
   isPaused = !isPaused;
 };
 
-document.getElementById('newGame').onclick = function(){
+document.getElementById("newGame").onclick = function () {
   isGameOver = false;
   isPaused = false;
-  var variableDisplay = document.getElementById('scoreDisplay');
+  var variableDisplay = document.getElementById("scoreDisplay");
   variableDisplay.textContent = 4;
-  oneSquareSize = parseInt(document.getElementById('squareSize').value);
-  if(oneSquareSize < 10){
+  oneSquareSize = parseInt(document.getElementById("squareSize").value);
+  if (oneSquareSize < 10) {
     oneSquareSize = 10;
-    document.getElementById('squareSize').value = 10;
-  }else if(oneSquareSize > 100){
+    document.getElementById("squareSize").value = 10;
+  } else if (oneSquareSize > 100) {
     oneSquareSize = 100;
-    document.getElementById('squareSize').value = 100;
+    document.getElementById("squareSize").value = 100;
   }
-  gameFieldFullNumber = (canvasWidth / oneSquareSize) * (canvasHeight / oneSquareSize);
+  gameFieldFullNumber =
+    (canvasWidth / oneSquareSize) * (canvasHeight / oneSquareSize);
   snake = new Snake(oneSquareSize, canvasWidth, canvasHeight);
   food = new Food(oneSquareSize, gameFieldFullNumber);
   isGridON = false;
-  console.log('Starting new game...', oneSquareSize);
+  console.log("Starting new game...", oneSquareSize);
 };
--- GAMES/JS_Canvas_Snake/snake.js
+++ GAMES/JS_Canvas_Snake/snake.js
@@ -1,7 +1,7 @@
-import {Cell} from './cell.js'
+import { Cell } from "./cell.js";
 
-export class Snake{
-  constructor( squareSize, canvasWidth, canvasHeight){
+export class Snake {
+  constructor(squareSize, canvasWidth, canvasHeight) {
     this.squareSize = squareSize;
     this.xLocation = this.squareSize * 3; // location of head - movement speed
     this.yLocation = 0;
@@ -9,51 +9,62 @@ export class Snake{
     this.canvasHeight = canvasHeight;
     this.xSpeed = 0;
     this.ySpeed = 0;
-    this.tail = [new Cell(0,0), new Cell( this.squareSize, 0), new Cell( this.squareSize*2, 0)];
+    this.tail = [
+      new Cell(0, 0),
+      new Cell(this.squareSize, 0),
+      new Cell(this.squareSize * 2, 0),
+    ];
+    this.snakeMoveDir = "right";
   }
 
-  update () {
+  update() {
     this.xLocation += this.xSpeed;
     this.yLocation += this.ySpeed;
   }
 
-  draw(context, color){
+  draw(context, color) {
     context.fillStyle = color;
-    if(this.xLocation + this.squareSize > this.canvasWidth){
+    if (this.xLocation + this.squareSize > this.canvasWidth) {
       this.xLocation = 0;
-    }else if (this.xLocation < 0){
-      this.xLocation = this.canvasWidth - this.squareSize ;
-    }else if (this.yLocation < 0){
-      this.yLocation = this.canvasHeight - this.squareSize ;
-    }else if (this.yLocation + this.squareSize > this.canvasHeight){
+    } else if (this.xLocation < 0) {
+      this.xLocation = this.canvasWidth - this.squareSize;
+    } else if (this.yLocation < 0) {
+      this.yLocation = this.canvasHeight - this.squareSize;
+    } else if (this.yLocation + this.squareSize > this.canvasHeight) {
       this.yLocation = 0;
     }
-    context.fillRect(this.xLocation, this.yLocation, this.squareSize, this.squareSize);
+    context.fillRect(
+      this.xLocation,
+      this.yLocation,
+      this.squareSize,
+      this.squareSize
+    );
+    this.drawSnakeEyes(context);
     this.tail.push(new Cell(this.xLocation, this.yLocation));
   }
 
-  drawTail(ctx, color){
+  drawTail(ctx, color) {
     // if(this.xTEST > 0 || this.yTEST > 0){
-      this.tail.forEach(cEll => {
-        ctx.fillStyle = color;
-        ctx.fillRect(cEll.x, cEll.y, this.squareSize, this.squareSize);
-      });
+    this.tail.forEach((cEll) => {
+      ctx.fillStyle = color;
+      ctx.fillRect(cEll.x, cEll.y, this.squareSize, this.squareSize);
+    });
     // }
   }
 
-  updateTail(){
+  updateTail() {
     // Add element at BEGINNING
     this.tail.unshift(new Cell(this.tail[0].x, this.tail[0].y));
   }
 
-  removeFirstElementFromTail(){
+  removeFirstElementFromTail() {
     this.tail.shift();
   }
 
-  isSnakeHeadCrashedInTail(){
+  isSnakeHeadCrashedInTail() {
     for (let index = 0; index < this.tail.slice(0, -2).length; index++) {
       const element = this.tail[index];
-      if(element.x === this.xLocation && element.y === this.yLocation){
+      if (element.x === this.xLocation && element.y === this.yLocation) {
         return true;
       }
     }
@@ -61,41 +72,97 @@ export class Snake{
     return false;
   }
 
-  changeDirection(dirKey){
+  changeDirection(dirKey) {
     // keys = w, s, a, d OR Up, Down, Left, Right
     switch (dirKey) {
-      case 'Up':
-      case 'w':
-        if(this.ySpeed !== this.squareSize * 1){
+      case "Up":
+      case "w":
+        if (this.ySpeed !== this.squareSize * 1) {
           this.xSpeed = 0;
           this.ySpeed = this.squareSize * -1;
+          this.snakeMoveDir = "up";
         }
         break;
-      case 'Down':
-      case 's':
-        if(this.ySpeed !== this.squareSize * -1){
+      case "Down":
+      case "s":
+        if (this.ySpeed !== this.squareSize * -1) {
           this.xSpeed = 0;
           this.ySpeed = this.squareSize * 1;
+          this.snakeMoveDir = "down";
         }
         break;
-      case 'Left':
-      case 'a':
-        if(this.xSpeed !== this.squareSize * 1){
+      case "Left":
+      case "a":
+        if (this.xSpeed !== this.squareSize * 1) {
           this.ySpeed = 0;
-          this.xSpeed = this.squareSize * -1
+          this.xSpeed = this.squareSize * -1;
+          this.snakeMoveDir = "left";
         }
         break;
-      case 'Right':
-      case 'd':
-        if(this.xSpeed !== this.squareSize * -1){
+      case "Right":
+      case "d":
+        if (this.xSpeed !== this.squareSize * -1) {
           this.ySpeed = 0;
           this.xSpeed = this.squareSize * 1;
+          this.snakeMoveDir = "right";
         }
         break;
-      case 'x':
+      case "x":
         this.tail.unshift(new Cell(this.tail[0].x, this.tail[0].y));
       default:
         break;
     }
   }
-};
+
+  drawSnakeEyes(context) {
+    context.fillStyle = "white";
+    const eyeSize = this.squareSize / 5;
+    if (this.snakeMoveDir === "right") {
+      const leftEyeLoc = {
+        x: this.xLocation + eyeSize * 3,
+        y: this.yLocation + eyeSize,
+      };
+      const rightEyeLoc = {
+        x: this.xLocation + eyeSize * 3,
+        y: this.yLocation + eyeSize * 3,
+      };
+      this.drawEyesHelper(context, leftEyeLoc, rightEyeLoc, eyeSize);
+    } else if (this.snakeMoveDir === "left") {
+      const leftEyeLoc = {
+        x: this.xLocation + eyeSize,
+        y: this.yLocation + eyeSize,
+      };
+      const rightEyeLoc = {
+        x: this.xLocation + eyeSize,
+        y: this.yLocation + eyeSize * 3,
+      };
+      this.drawEyesHelper(context, leftEyeLoc, rightEyeLoc, eyeSize);
+    } else if (this.snakeMoveDir === "up") {
+      const leftEyeLoc = {
+        x: this.xLocation + eyeSize,
+        y: this.yLocation + eyeSize,
+      };
+      const rightEyeLoc = {
+        x: this.xLocation + eyeSize * 3,
+        y: this.yLocation + eyeSize,
+      };
+      this.drawEyesHelper(context, leftEyeLoc, rightEyeLoc, eyeSize);
+    } else if (this.snakeMoveDir === "down") {
+      const leftEyeLoc = {
+        x: this.xLocation + eyeSize,
+        y: this.yLocation + eyeSize * 3,
+      };
+      const rightEyeLoc = {
+        x: this.xLocation + eyeSize * 3,
+        y: this.yLocation + eyeSize * 3,
+      };
+      this.drawEyesHelper(context, leftEyeLoc, rightEyeLoc, eyeSize);
+    }
+    // console.log(this.snakeMoveDir);
+  }
+
+  drawEyesHelper(context, leftEye, rightEye, size) {
+    context.fillRect(leftEye.x, leftEye.y, size, size);
+    context.fillRect(rightEye.x, rightEye.y, size, size);
+  }
+}
