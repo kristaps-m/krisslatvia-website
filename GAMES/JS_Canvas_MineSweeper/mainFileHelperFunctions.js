e8c85945f24efaec3747ccd721ee80b8dd40882c
Kristaps-M <kristapsmitins@gmail.com>
2025-02-14 16:29:51 +0200

    css for mobile minesweeper

---
+++ GAMES/JS_Canvas_MineSweeper/mainFileHelperFunctions.js
@@ -0,0 +1,40 @@
+function transformArray(theArray){
+  let newArray = []
+  theArray.forEach((c) => {
+    newArray.push({squareRender: {...c}});
+  })
+
+  return newArray;
+}
+
+// GAME TIMER
+function updateGameTimer(){
+  CTX_TIMER.font = "bold 25px Comic Sans MS";
+  CTX_TIMER.textAlign = "center"; CTX_TIMER.fillStyle = "red";
+  const textX = 50;
+  const textY = 25;
+  CTX_TIMER.fillText(gameTimer, textX, textY);
+}
+
+// check cheat
+function cheatIfCheatEntered(){
+  if(cheat.join("").includes("kivi apelsins")){
+    return true;
+  }
+
+  return false;
+}
+
+function toggleIsFirstClickMine(){
+  withFirstClickYouCanHitMine = !withFirstClickYouCanHitMine;
+  let firstClick = document.getElementById('isFirstClickMine');
+  firstClick.textContent = `${withFirstClickYouCanHitMine}`;
+}
+
+function toggleDigforminesOrFlag() {
+  doesClickDigs = !doesClickDigs;
+  let digOrFlag = document.getElementById('digOrFlag');
+  digOrFlag.style.color = doesClickDigs ? "black" : "red";
+  digOrFlag.style.backgroundColor = doesClickDigs ? "lightsalmon" : "lightgreen";
+  digOrFlag.textContent = doesClickDigs ? `DIG` : `FLAG 🚩`;
+}
