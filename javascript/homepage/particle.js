3b8e056837e3137aa05735f7cd3626100fce60d4
Kristaps-M <kristapsmitins@gmail.com>
2025-02-22 13:20:20 +0200

    update mobile!

--- javascript/homepage/particle.js
+++ javascript/homepage/particle.js
@@ -1,9 +1,8 @@
 class Particle {
-  constructor(x = 550 / 2, y = 400 / 2) {
+  constructor(x = 50, y = 50) {
     this.pos = new Position(x, y);
     this.rays = [];
-    for (let a = 0; a < 360; a+=2) {
-      // this.rays[i] = new Ray(this.pos.x, this.pos.y, degreesToRads(i));
+    for (let a = 0; a < 360; a+=1) {
       this.rays.push(new Ray(this.pos, degreesToRads(a), a));
     }
   }
@@ -30,7 +29,8 @@ class Particle {
       }
       if(closest){
         // if angle is straight (0, 90, 180, 270) then COLOR #FF8C00!
-        ctx.strokeStyle = ray.test % 90 === 0 ? "#FF8C00" : "yellow";
+        ctx.strokeStyle = ray.test % 90 === 0 ? "rgba(255, 238, 0, 0.4)" : "rgba(249, 255, 0, 0.6)";
+        // ctx.strokeStyle = "rgba(255, 240, 0, 0.3)";
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.moveTo(this.pos.x, this.pos.y);
--- javascript/homepage/ray.js
+++ javascript/homepage/ray.js
@@ -3,32 +3,24 @@ class Ray {
   constructor(pos, angle,t) {
     this.pos = pos;//new Position(x, y); // position
     this.test = t;
-    this.direction = vectorFromAngle(angle);//new Position(1, 0);
+    this.direction = vectorFromAngle(angle);
   }
 
   lookAt(x, y) {
     this.direction = new Position(x - this.pos.x, y - this.pos.y).normalizeVector();
-    // console.log(this.direction);
-    // this.direction.x = x;
-    // this.direction.y = y;
-    // this.direction.normalize();
   }
 
   drawRay(color = "yellow") {
     ctx.strokeStyle = color;
     ctx.lineWidth = 2;
     ctx.beginPath();
-    // ctx.translate(this.pos.x, this.pos.y);
-    // ctx.moveTo(0, 0);
     ctx.moveTo(this.pos.x, this.pos.y);
-    // ctx.lineTo(this.direction.x * 10, this.direction.y * 10);
     ctx.lineTo(this.pos.x + this.direction.x * 10, this.pos.y + this.direction.y * 10);
     ctx.stroke();
-    // console.log(this.x, this.y, this.direction.x, this.direction.y);
   }
 
   intersect(b) {
-    // boundry
+    // b = boundry
     const x1 = b.a.x,
       y1 = b.a.y;
     const x2 = b.b.x,
@@ -49,7 +41,6 @@ class Ray {
     if (theta > 0 && theta < 1 && u > 0) {
       const pt = new Position(x1 + theta * (x2 - x1), y1 + theta * (y2 - y1));
       return pt;
-      // return true;
     } else {
       return;
     }
