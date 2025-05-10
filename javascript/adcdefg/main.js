386a3555d39c9c7b92f56c4e8ecf1f418d6f57f0
Kristaps-M <kristapsmitins@gmail.com>
2025-05-10 20:54:05 +0300

    add the graph parabola player :D

--- javascript/adcdefg/main.js
+++ javascript/adcdefg/main.js
@@ -3,24 +3,41 @@ var c = document.getElementById("myCanvas3");
 var ctx = c.getContext("2d");
 var w = 800;
 var h = 800;
+const step = 10;
+const s = (n) => Math.floor(n / 10);
+let theYchange = 0;
+let theXchange = 0;
+let theSecretChange = 1;
 
-// Y axis
-ctx.beginPath();
-ctx.strokeStyle = "#000000";
-ctx.lineWidth = "1";
-ctx.moveTo(w / 2, 0);
-ctx.lineTo(w / 2, w);
-ctx.stroke();
+function drawXandYaxis() {
+  // Y axis
+  ctx.beginPath();
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = "2";
+  ctx.moveTo(w / 2, 0);
+  ctx.lineTo(w / 2, w);
+  ctx.stroke();
 
-// X axis
-ctx.beginPath();
-ctx.strokeStyle = "#000000";
-ctx.lineWidth = "1";
-ctx.moveTo(0, h / 2);
-ctx.lineTo(w, h / 2);
-ctx.stroke();
-ctx.closePath();
+  // X axis
+  ctx.beginPath();
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = "2";
+  ctx.moveTo(0, h / 2);
+  ctx.lineTo(w, h / 2);
+  ctx.stroke();
+  ctx.closePath();
+}
 
+// Parabola? y = x^2
+const xValuesList = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, -1, -2,
+  -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -16, -18, -20, -22, -24, -27, -30, -33, -36,
+  -39,
+];
+// const newXPluss = xValuesList.map((x) => x * 10);
+// const yValuesList = xValuesList.map((x) => Math.pow(x, 2));
+drawXandYaxis();
+drawTheGraph();
 // ctx.fillStyle = "red";
 // ctx.fillRect(20, 20, 20, 20);
 
@@ -35,15 +52,76 @@ for i in range -100 ... 100
 //   drawCircle(i, i, 3);
 // }
 
-// Parabola? y = x^2
-const xValuesList = [
-  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 18, 20, 22, 24, 27, 30, 33, 36, 39, -1, -2,
-  -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -16, -18, -20, -22, -24, -27, -30, -33, -36,
-  -39,
-];
-// const newXPluss = xValuesList.map((x) => x * 10);
-// const yValuesList = xValuesList.map((x) => Math.pow(x, 2));
-const yValuesList = xValuesList.map((x) => (1 / 9) * Math.pow(x + 5, 2) + 5);
+function selectSomething() {
+  theYchange = parseInt(document.getElementById("upOrDown").value);
+  let howFormulaLooks = document.getElementById("howFormulaLooks");
+  howFormulaLooks.textContent = `y = (1 / ${theSecretChange}) * (x - ${theXchange})^2 + ${theYchange}`;
+  console.log(theYchange);
+}
+
+function selectSomething2() {
+  theXchange = parseInt(document.getElementById("leftOrRight").value);
+  let howFormulaLooks = document.getElementById("howFormulaLooks");
+  howFormulaLooks.textContent = `y = (1 / ${theSecretChange}) * (x - ${theXchange})^2 + ${theYchange}`;
+  console.log(theXchange);
+}
+
+function selectSomething3() {
+  theSecretChange = parseFloat(document.getElementById("theSecret").value);
+  let howFormulaLooks = document.getElementById("howFormulaLooks");
+  howFormulaLooks.textContent = `y = (1 / ${theSecretChange}) * (x - ${theXchange})^2 + ${theYchange}`;
+  console.log(theSecretChange);
+}
+
+function drawTheGraph() {
+  ctx.clearRect(0, 0, w, h);
+  const firstLineStartPoints = [];
+  drawXandYaxis();
+  // THE FORMULA .........................
+  // from y = x^2
+  // to....
+  // y = (1 / theSecretChange) * (x - theXchange)^2 + theYchange
+  const yValuesList = xValuesList.map(
+    (x) => (1 / theSecretChange) * Math.pow(x - theXchange, 2) + theYchange
+  );
+  //----------------------------------------------------------------------------
+
+  // let howFormulaLooks = document.getElementById("howFormulaLooks");
+  // console.log(howFormulaLooks);
+
+  ctx.beginPath();
+  ctx.moveTo(firstLineStartPoints[0], firstLineStartPoints[1]);
+  for (let i = 0; i < w; i += step) {
+    for (let j = 0; j < h; j += step) {
+      ctx.font = "bold 12px Comic Sans MS";
+      // ctx.textAlign = "center";
+      ctx.fillStyle = "red";
+      const x = s(i - 400);
+      const y = s(j - 400) * -1;
+      // ctx.fillText(`*${x} ${y}`, i, j);
+      for (let index = 0; index < xValuesList.length; index++) {
+        if (
+          (xValuesList[index] === x && yValuesList[index] === y) ||
+          (Math.floor(xValuesList[index]) === x && Math.floor(yValuesList[index]) === y)
+        ) {
+          // ctx.fillText(`*${x} ${y}`, i, j);
+          // ctx.fillText(` ${xValuesList[index]}  ${yValuesList[index]}`, i, j);
+          // drawCircle(i, j, 3);
+          if (firstLineStartPoints.length === 0) {
+            firstLineStartPoints.push(i);
+            firstLineStartPoints.push(j);
+          }
+          // Draw Line?!
+          ctx.strokeStyle = "#000000";
+          ctx.lineWidth = "1";
+          ctx.lineTo(i, j);
+        }
+      }
+    }
+  }
+  ctx.stroke();
+  ctx.closePath();
+}
 
 // for (let i = 0; i < xValuesList.length; i++) {
 //   const x = newXPluss[i];
@@ -53,142 +131,6 @@ const yValuesList = xValuesList.map((x) => (1 / 9) * Math.pow(x + 5, 2) + 5);
 //   // ctx.textAlign = "center";
 //   ctx.fillText(`*${x} ${y}`, x + 400, (y - 400) * -1);
 // }
-
-const step = 10;
-const s = (n) => Math.floor(n / 10);
-const firstLineStartPoints = [];
-ctx.beginPath();
-ctx.moveTo(firstLineStartPoints[0], firstLineStartPoints[1]);
-for (let i = 0; i < w; i += step) {
-  for (let j = 0; j < h; j += step) {
-    ctx.font = "bold 12px Comic Sans MS";
-    // ctx.textAlign = "center";
-    ctx.fillStyle = "red";
-    const x = s(i - 400);
-    const y = s(j - 400) * -1;
-    // ctx.fillText(`*${x} ${y}`, i, j);
-    for (let index = 0; index < xValuesList.length; index++) {
-      if (
-        (xValuesList[index] === x && yValuesList[index] === y) ||
-        (Math.floor(xValuesList[index]) === x && Math.floor(yValuesList[index]) === y)
-      ) {
-        // ctx.fillText(`*${x} ${y}`, i, j);
-        // ctx.fillText(` ${xValuesList[index]}  ${yValuesList[index]}`, i, j);
-        // drawCircle(i, j, 3);
-        if (firstLineStartPoints.length === 0) {
-          firstLineStartPoints.push(i);
-          firstLineStartPoints.push(j);
-        }
-        // Draw Line?!
-        ctx.strokeStyle = "#000000";
-        ctx.lineWidth = "1";
-        ctx.lineTo(i, j);
-      }
-    }
-  }
-}
-ctx.stroke();
-ctx.closePath();
-
-// for (let i = -30; i < 30; i += 0.1) {
-//   drawCircle(i + 400, Math.pow(i, 2) + 400, 3);
-// }
-
-/*
-// Y axis
-ctx3.beginPath();
-ctx3.strokeStyle = "#000000";
-ctx3.lineWidth = "1";
-ctx3.moveTo(w / 2, 0);
-ctx3.lineTo(w / 2, w);
-ctx3.stroke();
-// X axis
-ctx3.beginPath();
-ctx3.strokeStyle = "#000000";
-ctx3.lineWidth = "1";
-ctx3.moveTo(0, h / 2);
-ctx3.lineTo(w, h / 2);
-ctx3.stroke();
-ctx3.closePath();
-
-//----
-var canvas = document.getElementById("myCanvas3");
-var ctx = canvas.getContext("2d");
-ctx.strokeStyle = "#000000";
-ctx.lineWidth = "2";
-
-var startX = 0;
-var startY = 0;
-var endX = 50;
-var endY = 50;
-var $startX = document.getElementById("sx");
-var $startY = document.getElementById("sy");
-var $endX = document.getElementById("ex");
-var $endY = document.getElementById("ey");
-
-$startX.value = startX;
-$startY.value = startY;
-$endX.value = endX;
-$endY.value = endY;
-
-draw();
-draw2();
-
-$startX.addEventListener(
-  "keyup",
-  function () {
-    startX = this.value;
-    draw();
-    draw2();
-  },
-  false
-);
-
-$startY.addEventListener(
-  "keyup",
-  function () {
-    startY = this.value;
-    draw();
-    draw2();
-  },
-  false
-);
-
-$endX.addEventListener(
-  "keyup",
-  function () {
-    endX = this.value;
-    draw();
-    draw2();
-  },
-  false
-);
-
-$endY.addEventListener(
-  "keyup",
-  function () {
-    endY = this.value;
-    draw();
-    draw2();
-  },
-  false
-);
-//-----------------
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillRect(startX, startY, Math.abs(endX - startX), Math.abs(endY - startY));
-}
-
-function draw2() {
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  // ctx.lineTo(endX+startX, endY+startY);
-  ctx.lineTo(endX, endY);
-  ctx.strokeStyle = "#ffffff";
-  ctx.stroke();
-  console.log(startX, startY, endX, endY);
-}
-*/
 
 function drawCircle(x, y, r) {
   // Circle
