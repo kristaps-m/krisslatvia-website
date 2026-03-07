9540de71881a46fb20cd018fad79594b93e24fd8
kristaps mitins <professional17@inbox.lv>
2026-03-07 21:09:03 +0200

    rotate both directions. it drops down.

--- GAMES/JS_Canvas_Tetris/script.js
+++ GAMES/JS_Canvas_Tetris/script.js
@@ -1,7 +1,23 @@
 const canvas = document.getElementById("tetris_canvas");
 const ctx = canvas.getContext("2d");
+const size = 20;
 
 // ctx.fillRect(10, 10, 10, 10);
+const gw = canvas.width / size;
+const gh = canvas.height / size;
+
+const gameField = []; // [][]
+
+for (let row = 0; row < gw; row++) {
+  const temp = [];
+  for (let col = 0; col < gh; col++) {
+    temp.push(0);
+  }
+  gameField.push(temp);
+}
+
+// gameField.map((x, i) => ([]));
+gameField[gameField.length - 1] = [1, 1, 1, 1, 1, 1, 1, 1];
 
 const square = [
   [0, 0, 0, 0],
@@ -44,7 +60,6 @@ const house = [
   ],
 ];
 
-const size = 20;
 class PuzzlePiece {
   constructor(sp, pieceArr) {
     this.sp = sp; // {x,y}
@@ -54,6 +69,7 @@ class PuzzlePiece {
   }
 
   draw() {
+    ctx.fillStyle = "black";
     this.activeArr.forEach((row, ri) => {
       row.forEach((elem, ci) => {
         if (elem == 1) {
@@ -68,20 +84,28 @@ class PuzzlePiece {
     });
   }
 
-  rotate() {
+  updateDown() {
+    this.sp.y += 1;
+    if (this.sp.y > 260 - size) {
+      this.sp.y = 260 - size;
+    }
+  }
+
+  rotateClockwise() {
     // 00, 01, 02, 03   30, 20, 10, 00
     // 10, 11, 12, 13   31, 21, 11, 01
     // 20, 21, 22, 23   32, 22, 12, 02
     // 30, 31, 32, 33   33, 23, 13, 03
-    // for (let r = 0; r < this.arr.length; r++) {
-    //   for (let c = 0; c < this.arr[0].length; c++) {
-    //     // const e = array[r][c];
-    //     this.arr[r][c] = this.arr[3 - c][r];
-    //   }
-    // }
     this.activeArrIndex++;
     if (this.activeArrIndex >= 4) {
       this.activeArrIndex = 0;
+    }
+  }
+
+  rotateCounterClockwise() {
+    this.activeArrIndex--;
+    if (this.activeArrIndex < 0) {
+      this.activeArrIndex = 3;
     }
   }
 }
@@ -91,16 +115,33 @@ const ppl = new PuzzlePiece({ x: 80, y: 80 }, long);
 const pph = new PuzzlePiece({ x: 120, y: 10 }, house);
 
 window.addEventListener("keypress", (e) => {
-  pph.rotate();
   //   pph.activeArrIndex++;
+  if (e.key == "d") {
+    pph.rotateClockwise();
+  } else if (e.key == "a") {
+    pph.rotateCounterClockwise();
+  }
   pph.activeArr = pph.arr[pph.activeArrIndex];
   console.log(e.key);
 });
 
+function drawField() {
+  ctx.fillStyle = "lightgray";
+  for (let row = 0; row < gw; row++) {
+    for (let col = 0; col < gh; col++) {
+      if (gameField[col][row] == 1) {
+        ctx.fillRect(row * size + 1, col * size + 1, size - 2, size - 2);
+      }
+    }
+  }
+}
+
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   //   pps.draw();
+  drawField();
   pph.draw();
+  pph.updateDown();
   //   if (Math.random() > 0.95) {
   //     pph.rotate();
   //     console.log(pph.activeArrIndex);
