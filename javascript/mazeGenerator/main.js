339f49cf64d5da183aacdfcbddb68facc93efb68
Kristaps-M <kristapsmitins@gmail.com>
2026-04-19 22:16:03 +0300

    Fix reference

--- javascript/mazeGenerator/main.js
+++ javascript/mazeGenerator/main.js
@@ -9,6 +9,7 @@ function selectMazeAlgorithm() {
   const wilsonsControls = document.getElementById("wilsonsControls");
   const newMazeBtnWilsons = document.getElementById("newMazeBtnWilsons");
   const solveWilsonsMazeBtn = document.getElementById("solveWilsonsMazeBtn");
+  const referenceToOLC = document.getElementById("referenceToOLC");
 
   switch (selectMaze.value) {
     case "recursive_1":
@@ -17,6 +18,7 @@ function selectMazeAlgorithm() {
       otherMazeAlg = false;
       solveBtnRecursive_1.style.display = "inline-block";
       newMazeBtnRecursive_1.style.display = "inline-block";
+      referenceToOLC.style.display = "inline-block";
       if (wilsonsControls) wilsonsControls.style.display = "none";
       if (newMazeBtnWilsons) newMazeBtnWilsons.style.display = "none";
       if (solveWilsonsMazeBtn) solveWilsonsMazeBtn.style.display = "none";
@@ -27,6 +29,7 @@ function selectMazeAlgorithm() {
       otherMazeAlg = true;
       solveBtnRecursive_1.style.display = "none";
       newMazeBtnRecursive_1.style.display = "none";
+      referenceToOLC.style.display = "none";
       if (wilsonsControls) wilsonsControls.style.display = "inline";
       if (newMazeBtnWilsons) newMazeBtnWilsons.style.display = "inline-block";
       if (solveWilsonsMazeBtn) solveWilsonsMazeBtn.style.display = "inline-block";
