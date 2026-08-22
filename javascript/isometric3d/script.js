accea03358122d0f3140527d8d6ec651952d9f41
Kristaps M. <professiona17@inbox.lv>
2026-08-23 01:09:40 +0300

    Draw isometric 3d second grid.

--- javascript/isometric3d/script.js
+++ javascript/isometric3d/script.js
@@ -4,9 +4,11 @@ const W = 800;
 const H = 800;
 const ARRAY_W = 15;
 const ARRAY_H = 10;
-const RECT_EDGE_SIZE = 20;
+const RECT_EDGE_SIZE = 30;
+const HALF_E_SIZE = RECT_EDGE_SIZE / 2;
 const HEX_SIZE = 20;
 const GRID_OFF_SET_TO_CENTRE = 100;
+const ISOMETRIC_ARRAY_OF_SET = {x:3, y:4};
 
 c.width = W;
 c.height = H;
@@ -15,6 +17,9 @@ class Cell {
     constructor(row,col){
         this.row = row;
         this.col = col;
+        this.isAcive = false;
+        this.canvasX = 0;
+        this.canvasY = 0;
     }
 }
 
@@ -29,11 +34,15 @@ for (let col = 0; col < ARRAY_H; col++) {
 }
 console.log(theGrid);
 
-function drawRect(x,y,w,h) {
+function drawRect(x,y,w,h, isActive) {
     ctx.beginPath();
-    ctx.fillStyle = "red";
+    // ctx.fillStyle = "red";
     ctx.rect(x,y,w,h);
     ctx.lineWidth = 2;
+    if (isActive) {
+        ctx.fillStyle = "blue";
+        ctx.fill();
+    }
     ctx.stroke(); 
 }
 
@@ -45,20 +54,44 @@ function drawGrid() {
                 cell.row * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
                 cell.col * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
                 RECT_EDGE_SIZE,
-                RECT_EDGE_SIZE);
+                RECT_EDGE_SIZE,
+                cell.isAcive
+            );
         }
     } 
 }
 
+
+
+
 function drawIsometricProjection() {
-   for (let col = 0; col < ARRAY_H; col++) {
+    let startX = 0;
+    let startY = 0;
+    let startXhelp = 0;
+    let startYhelp = 0;
+    for (let col = 0; col < ARRAY_H; col++) {
         for (let row = 0; row < ARRAY_W; row++) {
             const cell = theGrid[col][row];
-            drawHexagon(
-                cell.row * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE * 3,
-                cell.col * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE * 5,
+            // drawHexagon(
+            //     cell.row * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE * 3,
+            //     cell.col * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE * 5,
+            // );
+            const projX = cell.row * RECT_EDGE_SIZE * 2 + GRID_OFF_SET_TO_CENTRE * ISOMETRIC_ARRAY_OF_SET.x;
+            const projY = cell.col * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE * ISOMETRIC_ARRAY_OF_SET.y;
+            drawTheThing(
+                projX - startX - startXhelp,
+                projY + startY - startYhelp,
+                cell.isAcive
             );
+            cell.canvasX = projX - startX - startXhelp;
+            cell.canvasY = projY + startY - startYhelp;
+            startX += RECT_EDGE_SIZE;
+            startY += HALF_E_SIZE;
         }
+        startX = 0;
+        startY = 0;
+        startXhelp += RECT_EDGE_SIZE;
+        startYhelp += HALF_E_SIZE;
     } 
 }
 
@@ -88,36 +121,78 @@ function drawHexagon(x, y, fillColor, strokeColor = "black") {
     ctx.stroke();
 }
 
+function drawTheThing(row, col, isActive) {
+    ctx.beginPath();
+    ctx.moveTo(row + RECT_EDGE_SIZE, col);
+    ctx.lineTo(row + RECT_EDGE_SIZE * 2, col + HALF_E_SIZE);
+    ctx.lineTo(row + RECT_EDGE_SIZE, col + RECT_EDGE_SIZE);
+    ctx.lineTo(row, col + HALF_E_SIZE);
+    ctx.lineTo(row + RECT_EDGE_SIZE, col);
+    if (isActive) {
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+    ctx.stroke();
+}
+
 drawGrid();
 drawIsometricProjection();
 
-window.addEventListener("mousemove", (e) => {
+window.addEventListener("click", (e) => {
     ctx.clearRect(0,0,W,H);
     const rect = c.getBoundingClientRect();
     // Calculate the click position relative to the canvas
     let theX = (event.clientX - rect.left) * (c.width / rect.width); // Normalize x
     let theY = (event.clientY - rect.top) * (c.height / rect.height); // Normalize y
     let rowIndxClick = Math.floor((theY - GRID_OFF_SET_TO_CENTRE) / RECT_EDGE_SIZE);
-    let colIndxClick = Math.floor((theX - GRID_OFF_SET_TO_CENTRE)/ RECT_EDGE_SIZE); //
-    // console.log(theX, theY, rowIndxClick, colIndxClick);
+    let colIndxClick = Math.floor((theX - GRID_OFF_SET_TO_CENTRE)/ RECT_EDGE_SIZE); 
+
     // if (theX > rowIndxClick * RECT_EDGE_SIZE - GRID_OFF_SET_TO_CENTRE &&
     //     theX < rowIndxClick * RECT_EDGE_SIZE - GRID_OFF_SET_TO_CENTRE &&
     //     theY > colIndxClick * RECT_EDGE_SIZE - GRID_OFF_SET_TO_CENTRE &&
     //     theY < colIndxClick * RECT_EDGE_SIZE - GRID_OFF_SET_TO_CENTRE 
     // ) {
-        ctx.fillStyle = "blue";
-        ctx.fillRect(
-            colIndxClick * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
-            rowIndxClick * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
-            RECT_EDGE_SIZE,
-            RECT_EDGE_SIZE
-        );
+    if (rowIndxClick >= 0 && colIndxClick >= 0 && rowIndxClick < ARRAY_H && colIndxClick < ARRAY_W) {
+        theGrid[rowIndxClick][colIndxClick].isAcive = true;
+    }
+
+    // get indexes for isemetric projection
+    // let pRowInx = Math.floor((theY - GRID_OFF_SET_TO_CENTRE  - GRID_OFF_SET_TO_CENTRE * ISOMETRIC_ARRAY_OF_SET.x) / RECT_EDGE_SIZE);
+    // let pColInx = Math.floor((theX - GRID_OFF_SET_TO_CENTRE * ISOMETRIC_ARRAY_OF_SET.y + GRID_OFF_SET_TO_CENTRE) / HALF_E_SIZE); 
+
+    // if (pRowInx >= 0 && pColInx >= 0 && pRowInx < ARRAY_H && pColInx < ARRAY_W) {
+    //     theGrid[pRowInx][pColInx].isAcive = true;
     // }
+    // console.log(theX, theY, rowIndxClick, colIndxClick,"----", pRowInx, pColInx);
+    //     ctx.fillStyle = "blue";
+    //     ctx.fillRect(
+    //         colIndxClick * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
+    //         rowIndxClick * RECT_EDGE_SIZE + GRID_OFF_SET_TO_CENTRE,
+    //         RECT_EDGE_SIZE,
+    //         RECT_EDGE_SIZE
+    //     );
+    // }
+   for (let col = 0; col < ARRAY_H; col++) {
+        for (let row = 0; row < ARRAY_W; row++) {
+            const cell = theGrid[col][row];
+            if (cell.canvasX > theX &&
+                cell.canvasY > theY &&
+                cell.canvasX < theX + RECT_EDGE_SIZE &&
+                cell.canvasY < theY + RECT_EDGE_SIZE
+            ) {
+                console.log(cell);
+                // cell.isAcive = true;
+                drawRect(cell.canvasX, cell.canvasY, RECT_EDGE_SIZE * 2, RECT_EDGE_SIZE);
+            }
+        }
+    }
+    console.log(theX, theY);
 })
 
 function animationLoop() {
     drawGrid();
     drawIsometricProjection();
+    // drawTheThing(20, 20);
     requestAnimationFrame(animationLoop);
 }
 
